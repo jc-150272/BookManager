4d8503db8dd2eebedcf667756e92ebdec77acf44
凌 松本 <150272@jc-21.jp>
2017-12-14 10:28:52 +0900

    がんばれ！！！

--- BookManager/BookManager/BookPage.xaml.cs
+++ BookManager/BookManager/BookPage.xaml.cs
@@ -18,15 +18,76 @@ namespace BookManager
             InitializeComponent();
 
             ObservableCollection<Book> items = new ObservableCollection<Book>();
-            items.Add(new Book { Name = "John Doe" });
-            items.Add(new Book { Name = "Jane Doe" });
-            items.Add(new Book { Name = "Sammy Doe" });
+            items.Add(new Book { Name = "John Doe" ,Value = 4.0 });
+            items.Add(new Book { Name = "Jane Doe" ,Value = 3.5 });
+            items.Add(new Book { Name = "Sammy Doe" ,Value = 2.5});
+
+            if(Value <= 0.25)
+            {
+                ValueImage = "value_0.gif";
+            }
+
+            else if(Value <= 0.75)
+            {
+                ValueImage = "value_0.5.gif";
+            }
+
+            else if (Value <= 0.125)
+            {
+                ValueImage = "value_1.gif";
+            }
+
+            else if (Value <= 0.175)
+            {
+                ValueImage = "value_1.5.gif";
+            }
+
+            else if (Value <= 0.225)
+            {
+                ValueImage = "value_2.gif";
+            }
+
+            else if (Value <= 0.275)
+            {
+                ValueImage = "value_2.5.gif";
+            }
+
+            else if (Value <= 0.325)
+            {
+                ValueImage = "value_3.gif";
+            }
+
+            else if (Value <= 0.375)
+            {
+                ValueImage = "value_3.5.gif";
+            }
+
+            else if (Value <= 0.425)
+            {
+                ValueImage = "value_4.gif";
+            }
+
+            else if (Value <= 0.475)
+            {
+                ValueImage = "value_4.5.gif";
+            }
+
+            else 
+            {
+                ValueImage = "value_5.gif";
+            }
+
             BookListView.ItemsSource = items;
+
         }
 
         public class Book
         {
             public string Name { get; set; }
+
+            public double Value { get; set; }
+
+            public string ValueImage { get; set; }
         }
     }
 }
